f4edbc2d514db82fbe5400c55687e36aa2002117
SvelteRust <oddharald@myhren.ai>
2025-07-26 12:22:24 +0300

    nice

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,5 @@
 use eframe::egui;
+use egui::{Button, TextEdit, vec2};
 
 #[derive(Default)]
 pub struct App {
@@ -8,18 +9,31 @@ pub struct App {
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
+            // Header
             ui.heading("Thought");
             ui.add_space(5.0);
+
+            // Text input area
+            let available_height = ui.available_height() - 25.0;
             let text_area = ui.add_sized(
-                ui.available_size(),
-                egui::TextEdit::multiline(&mut self.thought),
+                vec2(ui.available_width(), available_height),
+                TextEdit::multiline(&mut self.thought),
             );
             text_area.request_focus();
-            if !self.thought.is_empty() {
-                ui.add_space(5.0);
-                if ui.button("Publish").clicked() {
-                    println!("Thought published: {}", self.thought);
-                }
+            ui.add_space(5.0);
+
+            // Publish button
+            let button = Button::new("Publish");
+            let button_response = if self.thought.is_empty() {
+                ui.add_enabled_ui(false, |ui| {
+                    ui.add_sized(vec2(ui.available_width(), 0.0), button)
+                })
+                .inner
+            } else {
+                ui.add_sized(vec2(ui.available_width(), 0.0), button)
+            };
+            if button_response.clicked() {
+                println!("Thought published: {}", self.thought);
             }
         });
     }
